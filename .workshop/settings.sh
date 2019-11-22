b383d29d698850c5241bd825da8fb9a2b2997502
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-11-23 09:55:27 +1100

    Update workshop title and description.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,7 @@
 WORKSHOP_NAME=odo
 WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-developing-with-odo:latest
+WORKSHOP_TITLE="Developing applications with odo"
+WORKSHOP_DESCRIPTION="Developing applications on OpenShift as Mere Mortals"
 WORKSHOP_MEMORY=512Mi
 AUTH_USERNAME=workshop
 AUTH_PASSWORD=workshop
