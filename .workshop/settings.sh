00818126754595f581292b8cbb108791656c22f0
jdob <jdobies@redhat.com>
2020-03-30 14:47:33 -0400

    Pull the 2.0 image for the 2.0 branch

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,5 @@
 WORKSHOP_NAME=odo
-WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-developing-with-odo:latest
+WORKSHOP_IMAGE=quay.io/openshiftlabs/lab-developing-with-odo:2.0
 WORKSHOP_TITLE="Developing applications with odo"
 WORKSHOP_DESCRIPTION="Developing applications with odo"
 WORKSHOP_MEMORY=512Mi
